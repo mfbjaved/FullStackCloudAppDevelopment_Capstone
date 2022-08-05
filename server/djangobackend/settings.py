d0f9166b7035d218a19280ed214b7fbe27ad8826
Madiha <mfbjaved@gmail.com>
2022-08-04 19:51:40 -0700

    modified manifest.yml and settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost","mfbjcardjango.us-east.cf.appdomain.cloud","mfbjcardjango.us-east.mybluemix.net"]
 
 
 # Application definition
